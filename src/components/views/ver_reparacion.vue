cac8ac66fb9b1d33679337f5de468f3693705e0a
Mariano Martin Blanco <blancomariano.68@gmail.com>
2024-03-07 00:31:06 -0300

    commit new

---
+++ src/components/views/ver_reparacion.vue
@@ -0,0 +1,78 @@
+<template>
+    <template v-if="isLoading">
+      <div class="align-self-center mt-5">
+        <img :src="`/img/3Dteca.png`" alt="logo cargando">
+        <p>Cargando...</p>
+      </div>
+  </template>
+<template v-else>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container banner p-5">
+        <h1 class="display-4 subtitulo-white">REPARACI<span>O</span>NES</h1>
+      </div>
+    </div>
+    <div class="container my-5">
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="col" v-for="(item, index) in reparaciones" :key="index">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">{{item.problema}}</h2>
+                    <p class="card-text">Posible causa: {{item.causa}}</p>
+                    <p class="card-text">Posible solución: {{item.solucion}}</p>
+                </div>
+                <img :src="item.foto" class="card-img-top">
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+</template>
+<script>
+import 'regenerator-runtime/runtime'
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../services/firebase';
+export default {
+    name: 'cargar_reparacion',
+    data() {
+        return {
+            isLoading: true,
+          reparaciones: [],
+          reparacion: {
+            problma: '',
+            causa: '',
+            solucion: '',
+            foto: ''
+          }
+        }
+    },
+    methods:{
+    async obtenerDatos () { 
+      const querySnapshot = await getDocs(collection(db, "reparaciones"));
+        querySnapshot.forEach((doc) => {
+        let reparacion = doc.data()
+        reparacion.id = doc.id
+        this.reparaciones.push(reparacion)
+      });
+      this.isLoading = false;
+    }
+    },
+    mounted() {
+        this.obtenerDatos();
+    },
+}
+</script>
+
+<style>
+.card {
+    display: flex;
+    flex-direction: column;
+}
+
+.card-body {
+    order: 2;
+}
+
+.card-img-top {
+    order: 1;
+}
+</style>
